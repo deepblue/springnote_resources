d8c6337626a6d199838c2988b9c59614241bab94
iasandcb <iasandcb@8a3e2545-6861-4513-bbc0-502abaebb7ec>
2008-01-17 11:16:06 +0000

    Support getting raw data of an attachment.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/springnote/springnote_resources@49 8a3e2545-6861-4513-bbc0-502abaebb7ec

--- lib/springnote/attachment.rb
+++ lib/springnote/attachment.rb
@@ -1,5 +1,13 @@
 module Springnote
   class Attachment < Base
     set_prefix      '/pages/:relation_is_part_of/'
+    def get_raw_data
+      springnote_url = URI.parse(self.site)
+      http = Net::HTTP.new(springnote_url.host, springnote_url.port)
+      req = Net::HTTP::Get.new("/pages/#{relation_is_part_of}/attachments/#{self.id}")
+      req.basic_auth(self.site.user, self.site.password)
+      response = http.request(req)
+      response.body
+    end
   end
 end # module Springnote
