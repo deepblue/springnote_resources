5c3cc650ab9d55c0881ee9798862dedea34648f4
Dongyoon Lee <nine.cow@gmail.com>
2009-01-30 13:05:23 +0800

    can specify server_host, server_port
    Signed-off-by: Bryan Kang <byblue@gmail.com>

--- lib/springnote/attachment.rb
+++ lib/springnote/attachment.rb
@@ -3,7 +3,7 @@ class Springnote::Attachment < Springnote::Base
   include Springnote::ImportFile
     
   def source
-    http = Net::HTTP.new(Springnote::Configuration::SERVER_URL, 80)
+    http = Net::HTTP.new(Springnote::Configuration::server_host, Springnote::Configuration::server_port)
     req = Net::HTTP::Get.new(element_path[0..-5])
     req.basic_auth(Springnote::Base.site.user, Springnote::Base.site.password)
     response = http.request(req)
--- lib/springnote/configuration.rb
+++ lib/springnote/configuration.rb
@@ -8,7 +8,29 @@ module Springnote
     attr_accessor :access_token,   :access_secret
     attr_accessor :protocol, :domain
     
-    SERVER_URL = "api.springnote.com"
+    def server_url
+      if server_port != 80
+        [server_host, server_port].join(":")
+      else
+        server_host
+      end
+    end
+    
+    def server_host=(host)
+      @server_host = host
+    end
+    
+    def server_host
+      @server_host
+    end
+    
+    def server_port=(port)
+      @server_port = port
+    end
+    
+    def server_port
+      @server_port || 80
+    end
     
     def protocol
       @protocol || 'http'
@@ -38,8 +60,8 @@ module Springnote
     
     def site
       @app_key ? 
-        "#{protocol}://#{username}:#{password}@#{SERVER_URL}/" :
-        "#{protocol}://#{SERVER_URL}/"
+        "#{protocol}://#{username}:#{password}@#{server_url}/" :
+        "#{protocol}://#{server_url}/"
     end
     
     def token
